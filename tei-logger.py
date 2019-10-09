986503fd19d66def47b72487e99e7226302a189e
Bill Simpson <w.r.simpson@gmail.com>
2019-10-08 23:09:48 -0800

    initial commit

---
+++ tei-logger.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+#
+# Bill Simpson (wrsimpson@alaska.edu) 8 Oct 2019
+#
+# This logger captures data from TEI instruments on a RPi
+# It uses the serial port connections of these instruments.
+# It also uses the "tei" low driver for these instruments
+
+import tei
+import time
+import datetime
+
+# control variables
+write_interval_secs = 30 # will write a data line about this often
+
+time_exception_secs = 100 # if time shifts more than this, there will
+# be a time exception and a new file will begin
+
+flush_interval_secs = 75 # this is how often the file actually writes to disk
+
+# set time format for datetime string in file
+timeformat = '%Y-%m-%d %H:%M:%S'
+
+# specifics for time
+basevarnames = ['datetime']
+
+# specifics for instruments
+varnames = ['CO_ppb','CO_flow_lpm','CO_pres_torr','CO_temp_C','SO2_ppb','SO2_flow_lpm','SO2_pres_torr','SO2_temp_C']
+
+try:
+    co = tei.TEInst(48)
+    so2 = tei.TEInst(43)
+
+except:
+    print('Cannot open serial port connections to instruments')
+    exit(1)
+
+# create full header for file
+headernames = basevarnames + varnames
+
+# set last write monotonic time to now
+lastwrite_monotonic = time.monotonic()
+
+# indicate outfile is not open
+outfile_open = False
+
+while True:
+    # now read instrument data
+    serialvector = []
+    # get CO data
+    serialvector.append(str(co.conc()))
+    serialvector.append(str(co.flow()))
+    serialvector.append(str(co.pres()))
+    serialvector.append(str(co.temp()))
+    # now SO2
+    serialvector.append(str(so2.conc()))
+    serialvector.append(str(so2.flow()))
+    serialvector.append(str(so2.pres()))
+    serialvector.append(str(so2.temp()))
+
+    secs_since_write = time.monotonic() - lastwrite_monotonic
+    while secs_since_write < write_interval_secs:
+        time.sleep(0.5)
+        secs_since_write = time.monotonic() - lastwrite_monotonic
+
+    # write some new data
+    if not outfile_open:
+        outfilename = datetime.datetime.now().strftime('tei-log-%Y%m%dT%H%M%S.txt')
+        outfile = open(outfilename,'w')
+        # write the header line
+        outfile.write('\t'.join(headernames)+'\n')
+        outfile_open = True
+        # set last datetime to now
+        last_dt = datetime.datetime.now()
+        secs_since_write = 0
+        lastflush_monotonic = time.monotonic() 
+    # write the data line
+    pred_dt = last_dt + datetime.timedelta(seconds=secs_since_write)
+    # build the base data
+    basedata = []
+    basedata.append(pred_dt.strftime(timeformat))
+    # concatenate to total vector of base + serial vector
+    totalvector = basedata + serialvector
+    # write totaldata vector
+    outfile.write('\t'.join(totalvector)+'\n')
+    # output to console in case anybody is there
+    print('\t'.join(totalvector))
+    # check if time shifted by more than allowed
+    curr_dt = datetime.datetime.now()
+    diff_secs = (curr_dt - pred_dt).total_seconds()
+
+    if abs(diff_secs) > time_exception_secs:
+        exception_string = 'Time shift exception -- computer time is: '
+        exception_string += curr_dt.strftime(timeformat)
+        exception_string += ' predicted time was: '
+        exception_string += pred_dt.strftime(timeformat)
+        exception_string += ' seconds time shifted = '
+        exception_string += str(diff_secs)+'\n'
+        outfile.write(exception_string)
+        outfile.close()
+        outfile_open = False
+    else:
+        # if date changes, close the old file and let a new one open
+        if last_dt.date() < curr_dt.date():
+            outfile.close()
+            outfile_open = False
+
+    # set last_dt from current write time
+    last_dt = curr_dt
+    # set the lastwrite seconds to now
+    lastwrite_monotonic = time.monotonic() 
+
+    # check if we should flush (write the file to disk)
+    secs_since_flush = time.monotonic() - lastflush_monotonic
+    if secs_since_flush > flush_interval_secs:
+        outfile.flush()
+        lastflush_monotonic = time.monotonic() 
---
+++ tei.py
@@ -0,0 +1,95 @@
+# use python3
+#
+# Module for running Thermo Environmental Instrument (TEI)
+# instruments over serial interface
+#
+# wrsimpson@alaska.edu  
+# 21 Sep 2019
+#
+# Note that the instrument ID, you can use the instrument number
+# (e.g. 43 for SO2) or the binary number below.  The relationship
+# is that the instrument ID is 128 + the model number.  The code
+# interprets numbers <128 as model numbers and >128 as iids.
+#
+# for the TEI 42C = 0xAA
+# for the SO2 monitor (43c) = 0xAB
+# for the CO monitor (48c) = 0xB0
+# for the TEI 49C = 0xB1
+
+import serial
+from serial.tools.list_ports import comports
+import time
+
+# low level commands  
+def send_command(ser,iid,str):
+    time.sleep(0.2)  # needed to wait to get the analyzer ready to respond again
+    inst_msg = chr(iid) + str + '\r'
+    ser.write(inst_msg.encode())
+
+def read_response(ser):
+    response = ser.read_until(b'\r').decode()
+    # print ('len='+str(len(response))+'<'+response.strip()+'>')
+    return(response.strip())
+
+def parse_response(command,response):
+    # print('got <'+command+'> response <'+response+'>')
+    try:
+        return(float(response.partition(command)[2].strip().split(' ')[0]))
+    except:
+        return(float('NaN'))
+
+class TEInst:
+
+    def __init__(self, instrumentID):
+        if (instrumentID < 128):
+            instrumentID += 128
+        self.iid = instrumentID
+        self.ser = None
+        if self.iid == 0xAB:  # SO2 monitor
+            self.conc_command = 'so2'
+        else:
+            self.conc_command = 'co'
+
+        for try_port in comports():
+            print('trying port '+try_port.device)
+            self.ser = serial.Serial(port=try_port.device, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1, xonxoff=False, rtscts=False, dsrdtr=False)
+            send_command(self.ser,self.iid,'instr name')
+            response = read_response(self.ser)
+            print('got response <'+response+'>')
+            if response:
+                print('port '+try_port.device+' matched instrument ID')
+                break
+            else:
+                self.ser = None
+                
+    def conc(self):
+        command = self.conc_command
+        
+        if self.ser:
+            send_command(self.ser,self.iid,command)
+            response = read_response(self.ser)
+            return(parse_response(command,response))
+
+    def flow(self):
+        command = 'flow'
+        
+        if self.ser:
+            send_command(self.ser,self.iid,command)
+            response = read_response(self.ser)
+            return(parse_response(command,response))
+        
+    def pres(self):
+        command = 'pres'
+        
+        if self.ser:
+            send_command(self.ser,self.iid,command)
+            response = read_response(self.ser)
+            return(parse_response(command,response))       
+        
+    def temp(self):
+        command = 'internal temp'
+        
+        if self.ser:
+            send_command(self.ser,self.iid,command)
+            response = read_response(self.ser)
+            return(parse_response(command,response))       
